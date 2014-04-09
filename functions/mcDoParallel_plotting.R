cb6effeb4df0a35ee8b941cb7e22b1c4b955dc14
przemol <pstempor@gmail.com>
2014-04-09 18:27:57 +0100

    FEATURES: new clustering algorithms - hierarchical and superSOM; heatmap order exported along with clusters; saved datasets are downloadable; clicking row/column toggles the checkboxes IMPROVEMENTS: Local functions loaded from "LOCAL" directory; mceval function used for parallel calculations; K-means clustering simplified BUGFIX: colors are matched case insensitive e.g. #ffffff matches #FFFFFF

--- functions/mcDoParallel_plotting.R
+++ functions/mcDoParallel_plotting.R
@@ -1,37 +1,5 @@
 mcDoParallel <- quote({
   
-  mceval <- function(do, do.iter=NULL, do.final=NULL) {
-
-    if(is.null(isolate(values$proc))) {
-      values$proc <- parallel::mcparallel(do)
-      invalidateLater(100, session)
-      
-    } else if ( parallel:::selectChildren(isolate(values$proc)) == parallel:::processID(isolate(values$proc)) ) {
-      res <- parallel::mccollect(isolate(values$proc), wait=FALSE)[[1]]
-      
-        if(class(res) == 'try-error' ) {
-          parallel::mccollect( isolate(values$proc) ); values$proc <- NULL 
-          session$sendCustomMessage("jsAlert", res); session$sendCustomMessage("jsExec", "$('#progressModal').modal('hide');")
-          
-        } else if ( is.null(res) ) {
-          parallel::mccollect( isolate(values$proc) ); values$proc <- NULL 
-          session$sendCustomMessage("jsExec", "$('#progressModal').modal('hide'); alert('Job canceled.');")
-          
-        } else {
-          parallel::mccollect( isolate(values$proc) )
-          values$proc <- NULL 
-          session$sendCustomMessage("jsExec", "$('#progressModal').modal('hide').find('#summary2').text('')")      
-          eval( do.final )
-    
-        }
-      
-    } else { 
-      eval( do.iter ) 
-      invalidateLater(100, session); 
-    }  
-  }
-  
-  
   if( (input$reactive) ) {
     #common
     is.null(list(
