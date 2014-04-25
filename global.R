7ff256a0a0c20098955d0887654806ce348af907
przemol <pstempor@gmail.com>
2014-04-25 18:43:45 +0100

    BugFix: Empty BSGenome installation does not produce error, chromosome names are set or "sel" rather than "gr", making it work properly for anchored plots

--- global.R
+++ global.R
@@ -1,3 +1,4 @@
 require(BSgenome)
 GENOMES <- BSgenome:::installed.genomes(splitNameParts=TRUE)$provider_version
-names(GENOMES) <- gsub('^BSgenome.', '', BSgenome:::installed.genomes())
+if( length(GENOMES) ) 
+  names(GENOMES) <- gsub('^BSgenome.', '', BSgenome:::installed.genomes())
