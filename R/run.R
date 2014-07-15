7fb2933d9760aa6f00e7a8c69327aa9beeb0dc9d
przemol <pstempor@gmail.com>
2014-07-15 17:27:44 +0100

    Roxygen documentation; "run" function replace "seqplots", user initiated exit keep path

--- R/run.R
+++ R/run.R
@@ -1,8 +1,27 @@
-seqplots <- function(root = file.path(path.expand("~"), "GFplots_data"), debug = FALSE) {
-  
+#' SeqPlots innitiation
+#'
+#' This function is called to initaite SeqPlots and call web browser graphical user interface.
+#' 
+#' @param root the path to data directory, it will be created if not existing
+#' @param debug run the SeqPlots in debyug mode, i.e. with web interface R console
+#' @return NULL
+#' 
+#' @details
+#' The default home dir is ~/SeqPlots data
+#'
+#' @references https://bitbucket.org/przemol/seqplots/wiki/Home
+#' @author Przemyslaw Stempor
+#' @keywords seqplots
+#' @export
+#' 
+#' @examples
+#' run()
+
+run <- function(root = file.path(path.expand("~"), "SeqPlots_data"), debug = FALSE) {
+    
   message('Starting...')
   oldwd <- getwd()
-  Sys.setenv(root=root, web=system.file('seqplots', package='seqplots'))
+  Sys.setenv(root=root, web=system.file('seqplots', package='seqplots'), seqplots_debug=debug)
   
   suppressPackageStartupMessages( require(RSQLite) )
   suppressPackageStartupMessages( require(shiny) )
@@ -20,5 +39,7 @@ seqplots <- function(root = file.path(path.expand("~"), "GFplots_data"), debug =
   message('\nData loaction: ', root)
   
   shiny::runApp(Sys.getenv('web'), launch.browser=TRUE)
+  
+  setwd(oldwd)
   
 }
---
+++ R/seqplots-package.R
@@ -0,0 +1,34 @@
+#' @docType package
+#' @name seqplots
+#' 
+#' @title 
+#' SeqPlots - An interactive tool for visualizing track signals and sequence 
+#' motif densities along genomic features using average plots and heatmaps.
+#' 
+#' @description
+#' SeqPlots is a web browser tool for plotting average track signals (e.g. read 
+#' coverage) and sequence motif densities over user specified genomic features. 
+#' The data can be visualized in linear plots with error estimates or as series 
+#' of heatmaps that can be sorted and clustered. The software can be run locally 
+#' on a desktop or deployed on a server and allows easy data sharing. SeqPlots 
+#' pre-calculates and stores binary result matrices, allowing rapid plot 
+#' generation. Plots can also be run in batch.
+#' 
+#' @references https://bitbucket.org/przemol/seqplots/wiki/Home
+#' @author Przemyslaw Stempor
+#' @details
+#' \tabular{ll}{
+#'  Package: \tab SeqPlots\cr
+#'  Type: \tab Package\cr
+#'  Version: \tab 0.9.2RC\cr
+#'  Date: \tab 2013-07-01\cr
+#'  License: \tab BSD\cr
+#'  LazyLoad: \tab yes\cr
+#' }
+#' 
+#' @keywords CHiP-seq genomics plotting sequencing
+#' @examples example(seqplots)
+#' 
+#' @importFrom  rtracklayer import export BigWigFile summary
+#' @exportPattern ^[[:alpha:]]+
+NULL
