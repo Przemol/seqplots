641d7008cfd86ba9d84b88485c6074f4fe180edc
przemol <pstempor@gmail.com>
2014-08-27 17:40:40 +0100

    More informative startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
     #Fix for annoying BiocGenerics namespace problems
     if(!"package:BiocGenerics" %in% search()) attachNamespace('BiocGenerics')
-    packageStartupMessage('SeqPlots loaded')
+    packageStartupMessage('SeqPlots ready, type "run()" to start web interface (GUI) or "?getPlotSeqArray" for console mode help.')
 }
