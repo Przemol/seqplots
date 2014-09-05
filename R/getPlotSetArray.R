55e36273c2441c7521f544db3c629482c11fa7a9
przemol <pstempor@gmail.com>
2014-09-05 15:07:38 +0100

    getPlotSetArray function restructured

--- R/getPlotSetArray.R
+++ R/getPlotSetArray.R
@@ -1,9 +1,10 @@
 #' Process genomic signal
 #'
-#' This function extracts and processes genomic signal from tracks and/or 
-#' motif data, calculates statistics and presents the data in nested list format.
-#' \code{\link{transform}} but for reordering a data frame by its columns.
-#' This saves a lot of typing!
+#' This function takes genomic cooridnates in BED or GFF format, 
+#' and extracts the signal from track files (BigWig) and/or 
+#' calculates motif density in these regions. Then it computs the statistics 
+#' required for average and heatmap plots, and returns the \code{\link{PlotSetArray}} 
+#' class.
 #'
 #' @param tracks list or vector of track paths (BigWig) and/or motif 
 #' setup structers (as \code{\link{list}})
@@ -12,19 +13,18 @@
 #' Homo sapiens
 #' @param bin binning window size in base pairs, default 1L
 #' @param rm0 remove 0 from mean/median calculation, default FALSE
-#' @param x1 upsterem distance in base pairs, default 500L
-#' @param x2 downsteram distance in base pairs, default 2000L
-#' @param xm anchored distance in base pairs, default 1000L
+#' @param xmin upsterem distance in base pairs, default 500L
+#' @param xmax downsteram distance in base pairs, default 2000L
+#' @param  anchored distance in base pairs, default 1000L
 #' @param type the type of the calculation, default, 'Point Features'
 #' @param add_heatmap return the heatmap data,  default FALSE
-#' @param cat3 function, that sends 1st level info to web interface, handeld by 
-#' "message" in console mode
-#' @param cat4 function, that sends 2nd level info to web interface, handeld by 
-#' "message" in console mode
-#' @param con connection to SQlite database storing genome information for files
-#' @return nested list: OUT[[FEATURE]][[TRACK/MOTIF]][[VALUE]]
+#' @param ignore_strand
 #' 
+#' @return \code{\link{PlotSetArray}}
+#' 
+#' @family plotFunctions
 #' @export
+#' 
 #' 
 #' @examples
 #' \dontrun{
@@ -36,8 +36,8 @@
 ###############################################################################
 
 getPlotSetArray <- function(tracks, features, refgenome, bin=10L, rm0=FALSE, 
-    ignore_strand=FALSE, x1=2000L, x2=2000L, xm=1000L, type='pf', 
-    add_heatmap=TRUE, cat3=message, cat4=message, con=NULL) {
+    ignore_strand=FALSE, xmin=2000L, xmax=2000L, xanchored=1000L, type='pf', 
+    add_heatmap=TRUE) {
     
     if( class(tracks) == "MotifSetup" ) tracks <- tracks$data
     
@@ -71,7 +71,7 @@ getPlotSetArray <- function(tracks, features, refgenome, bin=10L, rm0=FALSE,
         proc <- list()
         for(i in 1:length(tracks) ) {
             
-            cat3(paste('Processing:', basename(j), '@', tracks[[i]][[1]], '[', k, '/',length(features)*length(tracks), ']'))
+            message(paste('Processing:', basename(j), '@', tracks[[i]][[1]], '[', k, '/',length(features)*length(tracks), ']'))
             
             if (ignore_strand)                strand(sel) <- '*'
             if( type == 'mf' ) sel <- resize(sel, 1, fix='center')
@@ -93,57 +93,57 @@ getPlotSetArray <- function(tracks, features, refgenome, bin=10L, rm0=FALSE,
             
             if ( (type == 'pf') | (type == 'mf') ) {
                 
-                gr <- GenomicRanges::promoters(sel, x1, x2)
-                all_ind  <- seq(-x1, x2, by=as.numeric(bin) )
+                gr <- GenomicRanges::promoters(sel, xmin, xmax)
+                all_ind  <- seq(-xmin, xmax, by=as.numeric(bin) )
                 
                 if( class(tracks[[i]]) == 'character' ) {
                     M <- extarct_matrix(track, gr, length(all_ind), ignore_strand)
                     
                 } else if ( class(tracks[[i]]) == 'list' ) {
                     
-                    cat4("Processing genome...")
+                    message("Processing genome...")
                     seqlengths(gr) <- seqlengths(GENOME)[seqlevels(gr)]
                     gr <- trim(gr)
                     
-                    cat4("Searching for motif...")
+                    message("Searching for motif...")
                     M <- getSF(GENOME, gr, pattern, seq_win, !add_heatmap, revcomp=revcomp)
                     if (!ignore_strand) M[as.character(strand(gr))=='-', ] <- M[as.character(strand(gr))=='-', ncol(M):1]
                     
-                    cat4("Binning the motif...")
+                    message("Binning the motif...")
                     M <-  t(apply(M, 1, function(x) approx(x, n=ceiling(ncol(M)/bin))$y ))        
                     
                 }
                 
             } else if (type == 'af') {
-                left_ind <- seq(-x1, -1, by=bin)
-                mid_ind <- seq(0, xm, by=bin)   
-                right_ind <- seq(xm+bin, xm+x2, by=bin) 
+                left_ind <- seq(-xmin, -1, by=bin)
+                mid_ind <- seq(0, xanchored, by=bin)   
+                right_ind <- seq(xanchored+bin, xanchored+xmax, by=bin) 
                 all_ind <- c(left_ind, mid_ind, right_ind)
                 
                 if( class(tracks[[i]]) == 'character' ) {
-                    M.left <- extarct_matrix(track, flank(sel, x1, start=TRUE), length(left_ind), ignore_strand) #left
+                    M.left <- extarct_matrix(track, flank(sel, xmin, start=TRUE), length(left_ind), ignore_strand) #left
                     M.middle <- extarct_matrix(track, sel, length(mid_ind), ignore_strand) #middle    
-                    M.right <- extarct_matrix(track, flank(sel, x2, start=FALSE), length(right_ind), ignore_strand)  #right
+                    M.right <- extarct_matrix(track, flank(sel, xmax, start=FALSE), length(right_ind), ignore_strand)  #right
                     M <- cbind(M.left, M.middle, M.right)   
                     
                 } else if ( class(tracks[[i]]) == 'list' ) {
                     #MOTIF - left 
-                    cat4(paste0("MOTIF: Processing upsterem ", pattern, " motif..."))
-                    gr <- flank(sel, x1, start=TRUE); seqlengths(gr) <- seqlengths(GENOME)[seqlevels(gr)];
+                    message(paste0("MOTIF: Processing upsterem ", pattern, " motif..."))
+                    gr <- flank(sel, xmin, start=TRUE); seqlengths(gr) <- seqlengths(GENOME)[seqlevels(gr)];
                     M <- getSF(GENOME, trim(gr), pattern, seq_win, !add_heatmap, revcomp=revcomp)
                     if (!ignore_strand) M[as.character(strand(gr))=='-', ] <- M[as.character(strand(gr))=='-', ncol(M):1]
                     M.left <-  t(apply(M, 1, function(x) approx(x, n=length(left_ind))$y ))
                     
                     #MOTIF - middle
-                    cat4(paste0("MOTIF: Processing middle ", pattern, " motif..."))
+                    message(paste0("MOTIF: Processing middle ", pattern, " motif..."))
                     gr <- sel; seqlengths(gr) <- seqlengths(GENOME)[seqlevels(gr)];		
                     M <- getSF(GENOME, trim(gr), pattern, seq_win, !add_heatmap, revcomp=revcomp)
                     if (!ignore_strand) M[as.character(strand(gr))=='-', ] <- M[as.character(strand(gr))=='-', ncol(M):1]
                     M.middle <-  t(apply(M, 1, function(x) approx(x, n=length(mid_ind))$y ))
                     
                     #MOTIF - right
-                    cat4(paste0("MOTIF: Processing downsteream ", pattern, " motif..."))
-                    gr <- flank(sel, x1, start=FALSE); seqlengths(gr) <- seqlengths(GENOME)[seqlevels(gr)];
+                    message(paste0("MOTIF: Processing downsteream ", pattern, " motif..."))
+                    gr <- flank(sel, xmin, start=FALSE); seqlengths(gr) <- seqlengths(GENOME)[seqlevels(gr)];
                     M <- getSF(GENOME, trim(gr), pattern, seq_win, !add_heatmap, revcomp=revcomp)
                     if (!ignore_strand) M[as.character(strand(gr))=='-', ] <- M[as.character(strand(gr))=='-', ncol(M):1]
                     M.right <-  t(apply(M, 1, function(x) approx(x, n=length(right_ind))$y ))
@@ -153,14 +153,14 @@ getPlotSetArray <- function(tracks, features, refgenome, bin=10L, rm0=FALSE,
                 }
             }
             
-            #cat4("Calculeating means/stderr/95%CIs...")
+            #message("Calculeating means/stderr/95%CIs...")
             if (rm0) M[M==0] <- NA
             means 	<- colMeans(M, na.rm=TRUE) 
             stderror<- apply(M, 2, function (n) {sd(n, na.rm=T) / sqrt( sum(!is.na(n)) )})
             conint  <- apply(M, 2, function (n) {qt(0.975, sum(!is.na(n)) ) * sd(n, na.rm=T) / sqrt( sum(!is.na(n)) )})
             
-            #cat4("Exporting results...")
-            proc[[i]] <- list(means=means, stderror=stderror, conint=conint, all_ind=all_ind, e=if (type == 'af') xm else NULL,
+            #message("Exporting results...")
+            proc[[i]] <- list(means=means, stderror=stderror, conint=conint, all_ind=all_ind, e=if (type == 'af') xanchored else NULL,
                               desc=paste(sub("\\.(bw|BW)$", "", basename(tracks[[i]][[1]])), sub("\\.(gff|GFF)$", "", basename(j)), sep="\n@"),
                               heatmap=if (add_heatmap) M else NULL)
             k <- k+1
