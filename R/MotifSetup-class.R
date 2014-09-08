fdfd85a7a2a792b6c84dea48e64f582562901600
przemol <pstempor@gmail.com>
2014-09-08 18:56:59 +0100

    MotifSetup class restructured

--- R/MotifSetup-class.R
+++ R/MotifSetup-class.R
@@ -1,32 +1,39 @@
 #' MotifSetup Reference Class
 #' 
-#' @usage
-#' MotifSetup()$addMotif(pattern, window=200L, heatmap=TRUE, 
-#'             revcomp=TRUE, genome=NULL, name=pattern)
-#'
+#' This class is used to initialize motif plots and mix them with track plots. 
+#' 
+#' @details
+#' Usage note - the \code{addMotif} and \code{addBigWig} methods can be chained in following way:
+#' \code{MotifSetup()$addMotif("GAGA", window=200L)$addBigWig("path/to/file.bw")}
+#' 
 #' @field data a nested list holding the data
 #' @field annotations list of annotations
 #' 
 #' @family classes
 #' @export
 #' 
-MotifSetup <- setRefClass("MotifSetup", fields = list( data = "list", annotations = "list")  )
-MotifSetup$methods( nmotifs = function() {
-    "Prints number of motifs in class"
-    length(data) 
-})
-MotifSetup$methods( show = function() {
-    "Show method"
-    cat( 'MotifSetup with', nmotifs(), ' motifs.' ) 
-})
-MotifSetup$methods( addMotif = function(pattern, window=200L, heatmap=TRUE, revcomp=TRUE, genome=NULL, name=pattern ) {
-    "Adds new motif."
-    ind <- length(data)+1
-    data[[ind]] <<- list(pattern=pattern, window=window, heatmap=heatmap, revcomp=revcomp, genome=genome, name=name)
-    names(data)[[ind]] <<- name
-    return(.self)
-})
-MotifSetup$methods( addBigWig = function(file_path) {
-    "Adds new BigWig file."
-    data[[length(data)+1]] <<- file_path
-})
+MotifSetup <- setRefClass("MotifSetup", 
+    fields = list( data = "list", annotations = "list" ),
+    methods = list( 
+        nmotifs = function() {
+            "Prints number of motifs in class"
+            length(data) 
+        },
+        show = function() {
+            "Show method"
+            cat( 'MotifSetup with', nmotifs(), 'motifs/tracks.' ) 
+        },
+        addMotif = function(pattern, window=200L, heatmap=TRUE, revcomp=TRUE, genome=NULL, name=pattern ) {
+            "Adds new motif."
+            ind <- length(data)+1
+            data[[ind]] <<- list(pattern=pattern, window=window, heatmap=heatmap, revcomp=revcomp, genome=genome, name=name)
+            names(data)[[ind]] <<- name
+            return(.self)
+        },
+        addBigWig = function(file_path) {
+            "Adds new BigWig file."
+            data[[length(data)+1]] <<- file_path
+            return(.self)
+        }
+    )
+)
