d3c89f17efe454c32d0b24bf381e9bf29c7827ec
przemol <pstempor@gmail.com>
2014-09-10 20:58:37 +0100

    vignette cleanups

--- inst/doc/SeqPlots.R
+++
@@ -1,9 +0,0 @@
-### R code from vignette source 'SeqPlots.Rnw'
-### Encoding: UTF-8
-
-###################################################
-### code chunk number 1: style-Sweave
-###################################################
-BiocStyle::latex()
-
-
