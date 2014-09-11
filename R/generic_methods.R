95b40fa21dd63a8e34fb2651476418e6dc048521
przemol <pstempor@gmail.com>
2014-09-11 02:48:07 +0100

    docs for generic methods

--- R/generic_methods.R
+++ R/generic_methods.R
@@ -1,30 +1,49 @@
-#' Generic method for SeqPlots package
-#'
+#' Generic operators acting on \code{\link{PlotSetArray}}, 
+#' \code{\link{PlotSetList}} or \code{\link{PlotSetPair}} to extract pairs 
+#' or flatten the data structure.
+#' 
+#' @param x an object of class \code{\link{PlotSetArray}}, 
+#'  \code{\link{PlotSetList}} or \code{\link{PlotSetPair}} 
+#' @param j see description for \code{i}
+#' @param ... see description for \code{i}
+#' 
+#' @inheritParams base::Extract
 #' @name seqplots-generic
-#' @seealso Extract
+#' @seealso \code{\link[base]{Extract}}
 NULL
 
 
 #' Generic plot function for SeqPlots package calsses
 #' 
-#' @family PlotFunctions
+#' @param x This argument should be one of SeqPlots classes:
+#'  \code{\link{PlotSetArray}}, \code{\link{PlotSetList}} or
+#'  \code{\link{PlotSetPair}}
+#' @param y For plotting SeqPlots classes this argument is ignored, used for 
+#'  default functionality of \code{\link[graphics]{plot}} function.
+#' @param what This argument takes a character determining if avareange 
+#'  plot (\code{"a"}, default) or heatmap (\code{"h"}) will be plotted.
+#' @param ... Other parameters controlong the plot, see \code{\link{plotAverage}}
+#'  for avareange plot and \code{\link{plotHeatmap}} for heatmaps.
+#'  
+#' @seealso 
+#' \code{\link{getPlotSetArray}}
+#'  
+#' @family plotting functions
 #' @export
 #' 
 setGeneric('plot')
 
 #' @describeIn plot Method plot for signature 'PlotSetPair'
 #' @include PlotSetPair-class.R
-setMethod(plot, c("PlotSetPair"), function(x, ...) x$plot(...) )
+setMethod(plot, c("PlotSetPair"), function(x, what='a', ...) x$plot(what=what, ...) )
 
 #' @describeIn plot Method plot for signature 'PlotSetList'
 #' @include PlotSetList-class.R
-setMethod(plot,   c("PlotSetList"), function(x, ...) x$plot(...) )
-
+setMethod(plot,   c("PlotSetList"), function(x, what='a', ...) x$plot(what=what, ...) )
 
 #' @describeIn plot Method plot for signature 'PlotSetPair'
 #' @include PlotSetList-class.R
-setMethod(plot,   c("PlotSetArray"), function(x, ...) x$plot(...) )
-
+setMethod(plot,   c("PlotSetArray"), function(x, what='a', ...) x$plot(what=what, ...) )
 
 
 #' @rdname seqplots-generic
