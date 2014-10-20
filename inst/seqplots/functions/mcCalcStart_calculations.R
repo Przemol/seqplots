7bdeeaffa5111f7a808059d1cc643bea4e998acb
p.stempor <p.stempor@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-10-20 16:49:40 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 2 commits.
    Commit information:
    
    Commit id: 3ef9d0bd6913e79747f46bfdc0fecbd05c6b0ed5
    
        Midpoint Features plots added to GUI
    
    Committed by: przemol
    Author Name: przemol
    Commit date: 2014-10-20 17:48:31 +0100
    Author date: 2014-10-20 17:48:31 +0100
    
    Commit id: bc73899dbb194c54af242630894f2a693b398990
    
        seqplots::getPlotSetArray used in Shiny GUI for interval calculations
    
    Committed by: przemol
    Author Name: przemol
    Commit date: 2014-10-20 17:48:04 +0100
    Author date: 2014-10-20 17:48:04 +0100
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/seqplots@95776 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/seqplots/functions/mcCalcStart_calculations.R
+++ inst/seqplots/functions/mcCalcStart_calculations.R
@@ -11,10 +11,26 @@ mcCalcStart <- quote({
     cat3 <- function(x) { session$sendCustomMessage("jsExec", sprintf("$('#summary2').text('%s')", x)) }
     cat4 <- function(x) { session$sendCustomMessage("jsExec", sprintf("$('#summary3').text('%s')", x)) }
     if ( length( values$SFsetup ) > 0 | length( input$f_tracks ) > 0 ) {
-      procQuick(c(sort(input$f_tracks), values$SFsetup), sort(input$f_features),
-                x1 = input$plot_upstream, xm = input$anchored_downstream, x2 = input$plot_downstream,
-                type = input$plot_type, bin= as.numeric(input$BWbin),
-                cat3=cat3, cat4=cat4, rm0=input$rm0, ignore_strand=input$ignore_strand, add_heatmap=input$add_heatmap, con=con)
+        
+        out <- seqplots::getPlotSetArray(
+                tracks=c(file.path('files', sort(input$f_tracks)), values$SFsetup),
+                features=file.path('files', sort(input$f_features)),
+                refgenome=con,
+                xmin = input$plot_upstream, 
+                xanchored = input$anchored_downstream, 
+                xmax = input$plot_downstream,
+                type = gsub('^(.)[a-z]+ (.).+', '\\1\\2', tolower(input$plot_type)), 
+                bin= as.numeric(input$BWbin),
+                rm0=input$rm0,
+                ignore_strand=input$ignore_strand,
+                add_heatmap=input$add_heatmap,
+                verbose=TRUE, lvl1m=cat3, lvl2m=cat4
+            )       
+        out$data 
+      #procQuick(c(sort(input$f_tracks), values$SFsetup), sort(input$f_features),
+       #         x1 = input$plot_upstream, xm = input$anchored_downstream, x2 = input$plot_downstream,
+        #        type = input$plot_type, bin= as.numeric(input$BWbin),
+         #       cat3=cat3, cat4=cat4, rm0=input$rm0, ignore_strand=input$ignore_strand, add_heatmap=input$add_heatmap, con=con)
       
     }  else ( stop('Nothing to calculate!') )
   })
