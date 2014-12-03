8519e7c13d68d7c34da988556b5c8e6f07d099d9
Przemyslaw Stpempor <ps562@cam.ac.uk>
2014-12-03 18:28:15 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 1 commit.
    Commit information:
    
    Commit id: e4c8c8afbcfaf1daa555213adec8e4aacfbbf00a
    
        HTML grid have UI elements to select and batch change labels/colors/etc for rows and columns
    
    Committed by: przemol
    Author Name: przemol
    Commit date: 2014-12-03 18:27:59 +0000
    Author date: 2014-12-03 18:27:59 +0000
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/seqplots@97331 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/seqplots/www/js/afterHTMLgridrender.js
+++ inst/seqplots/www/js/afterHTMLgridrender.js
@@ -1,16 +1,71 @@
 if( !Modernizr.inputtypes.color ) { jscolor.bind() };
 
-$('#htmltab th b').click(function() { 
+$('.grid-all-toggle-btn').click(function() { 
   $('#htmltab input[value*="["]').click()
 });
 
-$('#htmltab th span span span').click(function() { 
-	var th = $(this).parent().parent().parent();
+$('.grid-all-select-btn').click(function() { 
+  $('#htmltab input[value*="["]').prop('checked', true).change()
+});
+
+
+$('.grid-all-remove-btn').click(function() { 
+  $('#htmltab input[value*="["]').prop('checked', false).change()
+});
+
+
+$('.grid-col-select-btn').click(function() { 
+	var th = $(this).parent();
 	$('input[value*="['+th .parent().children().index(th)+',"]').click()
 });
 
-$("#htmltab tr td:first-child").click(function() { 
+$(".grid-row-select-btn").click(function() { 
 	var num = $(this).parent().parent().children().index($(this).parent()) + 1;
 	$('input[value*=",'+num+']"]').click()
 
 });
+
+$(".toogle-sel-btn-vis").click(function() { 
+    $( "a[class*='grid-']" ).toggle();
+});
+
+$('a[data-toggle=tooltip]').tooltip();
+
+$(".rhdr").click(function() { 
+    var text = $(this).siblings().val();
+    var who = $(this).attr('data-who');
+    if( text == '' ) text = $(this).attr('data-fname');
+    if( who == 'inc' ) {
+        $(this).parents('tr').find('select[id*='+who+']').val(text).change();
+    } else {
+        $(this).parents('tr').find('input[id*='+who+']').val(text).change();
+    }
+
+});
+
+$('.hhdr').click(function() { 
+    var num = $(this).parents('th').index()+1;
+    var text = $(this).siblings().val();
+    var who = $(this).attr('data-who');
+    if( text == '' ) text = $(this).attr('data-fname');
+    if( who == 'inc' ) {
+        $('#plotTable tr > :nth-child('+num+') select[id*='+who+']').val(text).change();
+    } else {
+        $('#plotTable tr > :nth-child('+num+') input[id*='+who+']').val(text).change();
+    }
+     
+
+    
+});
+
+$('.hdr-numeric-auto-input').change(function() { 
+    var num = $(this).parents('th').index()+1;
+    var text = $(this).val();
+    var who = $(this).attr('data-who');
+    $('#plotTable tr > :nth-child('+num+') input[id*='+who+']').val(text).change();
+    
+});
+
+
+    // $('#plotTable tr > :nth-child('+num+') input[id*=label]')
+    //$('input[id*=",'+num+']"]').click();
