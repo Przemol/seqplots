e02c313fef9d6851fe66c7fe57ec386c7bd8b169
przemol <pstempor@gmail.com>
2015-08-10 17:03:48 +0100

    revent the mixed heatmaps feature - heatmap color set to default while sub-plot color is set to white

--- R/int_heatmapPlotWrapper.R
+++ R/int_heatmapPlotWrapper.R
@@ -73,7 +73,7 @@ heatmapPlotWrapper <- function(MAT, axhline=NULL, titles=rep('', length(MAT)),
     if(!is.null(dendro)) NP <- NP+1
     raster <- length(unique(diff(bins)))==1 & raster
     
-    #colvec[ grepl('#ffffff', colvec) ] <- NA
+    if( !is.null(colvec) ) colvec[ grepl('#FFFFFF', colvec) ] <- NA
     ncollevel = 1024
     if(length(colorspace)) {
         gcol <- colorRampPalette(colorspace)
