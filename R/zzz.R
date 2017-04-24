ad1cf2e327549ad733ea1185847a8fa22fc0cb8c
v.obenchain <v.obenchain@bc3139a8-67e5-0310-9ffc-ced21a209358>
2017-04-24 23:05:30 +0000

    deprecate seqplots
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/seqplots@129136 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,6 @@
 .onAttach <- function(libname, pkgname) {
-    #Fix for annoying BiocGenerics namespace problems
-    if(!"package:BiocGenerics" %in% search()) attachNamespace('BiocGenerics')
-    packageStartupMessage(paste(
-        'SeqPlots ready, type "run()" to start web interface (GUI) or',
-        '"?getPlotSetArray" for console mode help.'
-    ))
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.7")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
 }
