57020923a6ff35deb54476bfdb96510b930b2f3b
przemol <pstempor@gmail.com>
2019-06-12 11:04:44 +0100

    remove "Package 'seqplots' is deprecated and will be removed from Bioconductor version 3.11" message

--- R/zzz.R
+++ R/zzz.R
@@ -5,8 +5,4 @@
         'SeqPlots ready, type "run()" to start web interface (GUI) or',
         '"?getPlotSetArray" for console mode help.'
     ))
-    msg <- sprintf(
-        "Package '%s' is deprecated and will be removed from Bioconductor
-         version %s", pkgname, "3.11")
-    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
 }
